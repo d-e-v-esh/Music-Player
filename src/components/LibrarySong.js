92e9f007600c598d68c92d915e4403d959221111
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2020-11-14 19:08:38 +0530

    init

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -4,8 +4,10 @@ const LibrarySong = ({ song }) => {
     return (
         <div className="library-song">
             <img src={song.cover} alt={song.name} />
-            <h3>{song.name}</h3>
-            <h4>{song.artist}</h4>
+            <div className="song-description">
+                <h3>{song.name}</h3>
+                <h4>{song.artist}</h4>
+            </div>
         </div>
     );
 };
